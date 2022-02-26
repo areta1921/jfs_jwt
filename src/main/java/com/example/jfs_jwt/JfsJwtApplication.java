983e6b0f2a21f03f2615f41b1ca03cba9457a398
abraham <agetahun567@gmail.com>
2022-02-25 23:36:44 -0700

    maven spring boot

---
+++ src/main/java/com/example/jfs_jwt/JfsJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.jfs_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfsJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfsJwtApplication.class, args);
+	}
+
+}
